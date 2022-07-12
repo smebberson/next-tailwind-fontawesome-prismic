306771b0cad008b05b093b400ec01de34d096196
Scott Mebberson <74628+smebberson@users.noreply.github.com>
2022-07-12 12:16:28 +0930

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# next-tailwind-fontawesome
+
+An example setup using Next.js, Tailwind CSS and Font Awesome.
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Header from '../components/header';
+
+function HomePage() {
+    return <Header />;
+}
+
+export default HomePage;
